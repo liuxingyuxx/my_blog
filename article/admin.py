e865497388bb06d8cecb8a24a5c32a33d1a2c903
liuxingyuxx <liuxingyu19940812@gmail.com>
2018-03-18 08:17:51 +0800

    this is my_blog in shiyanlou

---
+++ article/admin.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from article.models import *
+
+
+# Register your models here.
+# class ArticleModel():
+
+
+
+admin.site.register(Article)
